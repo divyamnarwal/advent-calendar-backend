76687fad84d1b83ceb764f6743ec2d409d17753b
Divyam Narwal <developer@localhostpagal>
2026-01-20 20:00:32 +0530

    Day 1:Initialize Spring-boot backend with Postgresql and maven setup

---
+++ src/main/java/com/divyam/advent/AdventbackendApplication.java
@@ -0,0 +1,13 @@
+package com.divyam.advent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventbackendApplication.class, args);
+	}
+
+}
